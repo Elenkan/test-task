70d342b902951bd281e22f3e43fb35d45fb3e998
Елена <elenalecs@rambler.ru>
2020-12-06 21:06:45 +0400

    Доработка стилей, сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,41 @@ const sourcemap = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const server = require('browser-sync').create();
-gulp.task('css', function () {
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
+gulp.task('css', () => {
 	return gulp.src('source/less/style.less')
 	.pipe(plumber())
 	.pipe(sourcemap.init())
 	.pipe(less())
 	.pipe(postcss([
-		autoprefixer()]
-		))
+    autoprefixer()]))
+  .pipe(csso())
+  .pipe(rename('style.min.css'))
 	.pipe(sourcemap.write('.'))
-	.pipe(gulp.dest('source/css'));
+	.pipe(gulp.dest('build/css'));
 }
 	);
-gulp.task('server', function() {
+
+gulp.task('images', () => {
+  return gulp.src('source/img/**/*.{png,svg}')
+  .pipe(imagemin([
+    imagemin.svgo()]))
+  .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('server', () => {
 	server.init({
-		server: 'source/'
+		server: 'build/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
 	});
+
 	gulp
   .watch('source/less/**/*.less', gulp.series('css'))
   .on('change', server.reload);
@@ -33,4 +52,23 @@ gulp.task('server', function() {
   .watch('source/js/**/*.js')
   .on('change', server.reload);
 });
-gulp.task('start', gulp.series('css','server'));
+
+gulp.task('copy', () => {
+  return gulp.src([
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source/js/**',
+    'source/index.html'
+    ],
+    {
+      base: 'source'
+    })
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', () => {
+  return del('build');
+});
+
+gulp.task('build', gulp.series('clean', 'copy', 'images', 'css'));
+gulp.task('start', gulp.series('build','server'));
